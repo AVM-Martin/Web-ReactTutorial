8b7fd1e2bf8d24f74a2113d9c2958230ee997222
AVM.Martin <avmm.martin+github@gmail.com>
2019-05-07 02:34:19 +0700

    Updates

---
+++ precache-manifest.f70bc7cfc9945e9d9f4750b44a7532fb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55470e80a0e1e1a35f17ecdbee6184ab",
+    "url": "/react-trial/index.html"
+  },
+  {
+    "revision": "af79d80ca275f0dd74d5",
+    "url": "/react-trial/static/css/main.dbeee5b3.chunk.css"
+  },
+  {
+    "revision": "685d0c597e8419129599",
+    "url": "/react-trial/static/js/2.0e3f6a1a.chunk.js"
+  },
+  {
+    "revision": "af79d80ca275f0dd74d5",
+    "url": "/react-trial/static/js/main.678fca34.chunk.js"
+  },
+  {
+    "revision": "0dbc265fa73a0ba03846",
+    "url": "/react-trial/static/js/runtime~main.bdfd678c.js"
+  }
+]);
